9f7b13da39cc9d1aa10855bf10c1d6fd9be4fb36
yishain11 <yishain11@walla.co.il>
2024-07-11 15:49:51 +0300

    added format to test log

--- bot_functions/basic_functions.py
+++ bot_functions/basic_functions.py
@@ -1,11 +1,13 @@
 import requests
 import os
 import json
+from datetime import datetime
 
 server_url = os.environ["POC_TELEGRAM_SERVER"]
 
 async def test(update, context):
-    await update.message.reply_text('Hello! I am your bot. testing')
+    current_date = datetime.now().strftime("%d-%m-%Y")
+    await update.message.reply_text(f'Hello! I am your bot. testing in ${current_date}')
 
 async def ping_server(update, context):
     answer = requests.get(server_url)   
