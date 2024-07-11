a657d4a00a4b19b8cd1b7e07fc8bb36a3dd7779c
yishain11 <yishain11@walla.co.il>
2024-07-11 15:32:38 +0300

    added server endpoint in bot

--- bot_functions/basic_functions.py
+++ bot_functions/basic_functions.py
@@ -1,2 +1,13 @@
+import requests
+import os
+import json
+
+server_url = os.environ["POC_TELEGRAM_SERVER"]
+
 async def test(update, context):
     await update.message.reply_text('Hello! I am your bot. testing')
+
+async def ping_server(update, context):
+    answer = requests.get(server_url)   
+    await update.message.reply_text(json.dumps(answer))
+     
