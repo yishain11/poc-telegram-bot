40df8f84cd60c5d2b50e99adecd18b0938a94fc4
yishain11 <yishain11@walla.co.il>
2024-07-11 15:57:50 +0300

    added ip to test log

--- bot_functions/basic_functions.py
+++ bot_functions/basic_functions.py
@@ -7,7 +7,9 @@ server_url = os.environ["POC_TELEGRAM_SERVER"]
 
 async def test(update, context):
     current_date = datetime.now().strftime("%d-%m-%Y")
-    await update.message.reply_text(f'Hello! I am your bot. testing in ${current_date}')
+    response = requests.get('https://httpbin.org/ip')
+    ip_info = response.json()['origin']
+    await update.message.reply_text(f'Hello! I am your bot. testing in ${current_date}@{ip_info}')
 
 async def ping_server(update, context):
     answer = requests.get(server_url)   
